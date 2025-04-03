2ab3cc7a9453caa600e47e9b4f453eee4c89c121
Alexia Yailin Iracheta <yai.cheta@gmail.com>
2025-04-02 21:49:54 -0600

    Breeze and Blade
    I'm not sure what does this do, missed the class but it has to do with authentication of users.

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Universe;
-use App\Http\Controllers\GenderController;
-use App\Http\Controllers\UniverseController;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\SuperheroeController;
-
-
 
 Route::get('/', function () {
-    echo 'Hello! First Laravel application!';
-    /* echo '<pre>';
-    print_r(Universe::all());
-    echo '</pre>'; */
-    // Retrieve all universes from the database
-    // Universe::all();=> SELECT * FROM universes
-
-    dump(Universe::all());
-
-    //return view('welcome');
+    return view('welcome');
 });
 
-// Route::get('/gender',[GenderController::class,'index']);
-// Route::get('/universe',[UniverseController::class,'index']);
-//Generar rutas de manera automatica gracias a laravel :D 
-Route::resource('genders',GenderController::class);
-Route::resource('universes',UniverseController::class);
-Route::resource('genres',GenreController::class);
-Route::resource('superheroes',SuperheroeController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
